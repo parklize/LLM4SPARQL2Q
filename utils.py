c6bc089e83f64cad91a4e47aaa41717aa45f718a
Piao <guangyuan.piao@dell.com>
2025-03-19 13:36:01 +0000

    update utils

--- utils.py
+++ utils.py
@@ -22,15 +22,19 @@ def eval(res_dict: dict):
     return res_dict
 
 
-def eval_res(dir='results', model_name_or_path=None):
+def eval_res(dir='results', model_name_or_path=None, recompute=False):
     ''' 
     Combine all results 
     Print out the mean and std 
+
+    Args:
+        dir: results directory
+        model_name_or_path: model name or path for using BERT Score
+        recompute: whether to recompute BERT Score or use existing one if there is
     '''
     # Recompute
-    #bertscore = BERTScore(
-    #    model_name_or_path=model_name_or_path,
-    #)
+    if recompute:
+        bertscore = BERTScore()
 
     dataframes = list()
     for file in os.listdir(dir):
@@ -47,14 +51,15 @@ def eval_res(dir='results', model_name_or_path=None):
             if 'MISTRALFT' in file:
                 df['template'] = df['template'].str.replace('MISTRAL', 'MISTRALFT')
 
-            print(df)
-
-            #res = bertscore(
-            #    df['generated'].tolist(),
-            #    df['ground_truth'].tolist(),
-            #)
-            #for metric in ['f1', 'precision', 'recall']:
-            #    df[f'{metric}-BERTScore'] = res[metric].tolist()
+            #print(df)
+            
+            if recompute:
+                res = bertscore(
+                    df['generated'].tolist(),
+                    df['ground_truth'].tolist(),
+                )
+            for metric in ['f1', 'precision', 'recall']:
+                df[f'{metric}-BERTScore'] = res[metric].tolist()
 
             print(f'{file} -- df shape: {df.shape} -- {df["f1-BERTScore"].mean()}')
 
@@ -75,49 +80,59 @@ def eval_res(dir='results', model_name_or_path=None):
     combined_df = pd.concat(dataframes, ignore_index=True)
     combined_df = combined_df.reset_index(drop=True)
 
-    print(combined_df.shape)
-    print(combined_df.dtypes)
-    print(combined_df.head())
+    #print(combined_df.shape)
+    #print(combined_df.dtypes)
+    #print(combined_df.head())
     
     res = combined_df.groupby(
             ['hub_id', 'template']
     )[['precision-BERTScore', 'recall-BERTScore', 'f1-BERTScore']] \
     .agg(['mean'])
     
-    print(res)
-    print(res.to_latex(float_format="%.4f"))
+    #print(res)
+    #print(res.to_latex(float_format="%.4f"))
 
     
-    pre = combined_df[ \
-        (combined_df['template'] == 'BASIC0') \
-        & (combined_df['hub_id'] == 'meta-llama/Llama-2-7b-chat-hf') \
-    ]['f1-BERTScore'].tolist()
+    #pre = combined_df[ \
+    #    (combined_df['template'] == 'BASIC0') \
+    #    & (combined_df['hub_id'] == 'meta-llama/Llama-2-7b-chat-hf') \
+    #]['f1-BERTScore'].tolist()
 
-    ttest(
-        combined_df, 
-        pre_template='BASIC0', 
-        templates=['BASIC1', 'BASIC3', 'BASIC5', 'CoT0'], 
-        hub_id='meta-llama/Llama-2-7b-chat-hf'
-    )
-    ttest(
-        combined_df, 
-        pre_template='LLAMA3_BASIC0', 
-        templates=['LLAMA3_'+x for x in ['BASIC0', 'BASIC1', 'BASIC3', 'BASIC5', 'CoT0']], 
-        hub_id='meta-llama/Meta-Llama-3-8B-Instruct'
-    )
-    ttest(
-        combined_df, 
-        pre_template='MISTRAL_BASIC0', 
-        templates=['MISTRAL_'+x for x in ['BASIC0', 'BASIC1', 'BASIC3', 'BASIC5', 'CoT0']], 
-        hub_id='mistralai/Mistral-7B-Instruct-v0.2'
-    )
-    ttest(
-        combined_df, 
-        pre_template='MISTRALFT_BASIC0', 
-        templates=['MISTRALFT_'+x for x in ['BASIC0', 'BASIC1', 'BASIC3', 'BASIC5', 'CoT0']], 
-        hub_id='mistralai/Mistral-7B-Instruct-v0.2'
-    )
+    #ttest(
+    #    combined_df, 
+    #    pre_template='BASIC0', 
+    #    post_templates=['BASIC1', 'BASIC3', 'BASIC5', 'CoT0'], 
+    #    hub_id='meta-llama/Llama-2-7b-chat-hf'
+    #)
+    #ttest(
+    #    combined_df, 
+    #    pre_template='LLAMA3_BASIC0', 
+    #    post_templates=['LLAMA3_'+x for x in ['BASIC0', 'BASIC1', 'BASIC3', 'BASIC5', 'CoT0']], 
+    #    hub_id='meta-llama/Meta-Llama-3-8B-Instruct'
+    #)
+    #ttest(
+    #    combined_df, 
+    #    pre_template='MISTRAL_BASIC0', 
+    #    post_templates=['MISTRAL_'+x for x in ['BASIC0', 'BASIC1', 'BASIC3', 'BASIC5', 'CoT0']], 
+    #    hub_id='mistralai/Mistral-7B-Instruct-v0.2'
+    #)
+    #ttest(
+    #    combined_df, 
+    #    pre_template='MISTRALFT_BASIC0', 
+    #    post_templates=['MISTRALFT_'+x for x in ['BASIC0', 'BASIC1', 'BASIC3', 'BASIC5', 'CoT0']], 
+    #    hub_id='mistralai/Mistral-7B-Instruct-v0.2'
+    #)
 
+    print('Testing R1...')
+    for pre in ['LLAMA3_'+x for x in ['BASIC0', 'BASIC1', 'BASIC3', 'BASIC5',]]:
+        for post in ['R1_'+x for x in ['BASIC0', 'BASIC1', 'BASIC3', 'BASIC5',]]:
+            ttest(
+                combined_df,
+                pre_template=pre,
+                post_templates=[post],
+                hub_id='llama3.3-70B',
+                hub_id_post='R1'
+            )
 
     combined_df['query_len'] = combined_df['sparql'].str.split().str.len()
     plt.figure(figsize=(6, 4))
@@ -129,14 +144,15 @@ def eval_res(dir='results', model_name_or_path=None):
     # Save plot to a file
     plt.savefig('results/scatter_plot.png', dpi=300)
 
-def ttest(df: pd.DataFrame, pre_template: str, templates: list, hub_id: str) -> None:
+def ttest(df: pd.DataFrame, pre_template: str, post_templates: list, hub_id: str, hub_id_post: str = None) -> None:
     """ Print ttest results 
     
     Args:
         df: Dataframe containing all results
         pre_template: pre template to filter
-        templates: to filter dataframe for post treatments
+        post_templates: to filter dataframe for post treatments
         hub_id: to filter dataframe for post treatments
+        hub_id_post: in case to compare different model
     """
 
     pre = df[ \
@@ -145,13 +161,13 @@ def ttest(df: pd.DataFrame, pre_template: str, templates: list, hub_id: str) -> 
     ]['f1-BERTScore'].tolist()
 
     print('#'*90+' ')
-    for template in templates:
+    for template in post_templates:
         post = df[ \
             (df['template'] == template) \
-            & (df['hub_id'] == hub_id) \
+            & (df['hub_id'] == (hub_id_post if hub_id_post else hub_id)) \
         ]['f1-BERTScore'].tolist()
         print(len(pre), len(post))
-        print(hub_id, template, stats.ttest_rel(pre, post))
+        print(pre_template, template, stats.ttest_rel(pre, post))
 
 
 def clean_output(res: str, template: str) -> str:
@@ -172,6 +188,4 @@ def clean_output(res: str, template: str) -> str:
 
 
 if __name__ == '__main__':
-    eval_res(
-        #model_name_or_path='bert-base-uncased'
-    )
+    eval_res()
